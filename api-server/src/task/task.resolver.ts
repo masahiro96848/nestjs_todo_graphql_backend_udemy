0f75eaec3977e1f10109ab5de6380db79ce25b6e
masahiro96848 <feetwcpic9436@gmail.com>
2024-12-08 21:12:13 +0900

    taskのmodule、resolver、serviceを実装

--- api-server/src/task/task.resolver.ts
+++ api-server/src/task/task.resolver.ts
@@ -1,4 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { TaskService } from './task.service';
+import { Task } from './models/task.model';
 
 @Resolver()
-export class TaskResolver {}
+export class TaskResolver {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Query(() => [Task], { nullable: 'items' })
+  getTasks(): Task[] {
+    return this.taskService.getTasks();
+  }
+}
--- api-server/src/task/task.service.ts
+++ api-server/src/task/task.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './models/task.model';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  tasks: Task[] = [];
+
+  getTasks(): Task[] {
+    const task1 = new Task();
+    task1.id = 1;
+    task1.name = 'task1';
+    task1.description = 'task1の説明です';
+    this.tasks.push(task1);
+    return this.tasks;
+  }
+}
