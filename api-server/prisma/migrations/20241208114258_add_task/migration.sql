ce4731d1e1bfe240311ffca3008692886ddb38f7
masahiro96848 <feetwcpic9436@gmail.com>
2024-12-08 20:46:50 +0900

    Taskテーブルを作成。

---
+++ api-server/prisma/migrations/20241208114258_add_task/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `Task` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `createdAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updatedAt` TIMESTAMP(0) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
