020d89ab24e5418bc7c77b20fad4b8e3c3453526
masahiro96848 <feetwcpic9436@gmail.com>
2024-12-08 22:51:59 +0900

    userとtaskのリレーションの定義。タスクの作成と一覧取得機能の修正

--- api-server/src/task/dto/createTask.input.ts
+++ api-server/src/task/dto/createTask.input.ts
@@ -1,4 +1,4 @@
-import { Field, InputType } from '@nestjs/graphql';
+import { Field, InputType, Int } from '@nestjs/graphql';
 import { IsNotEmpty } from 'class-validator';
 
 @InputType()
@@ -9,4 +9,7 @@ export class CreateTaskInput {
 
   @Field({ nullable: true })
   description?: string;
+
+  @Field(() => Int)
+  userId: number;
 }
--- api-server/src/task/task.service.ts
+++ api-server/src/task/task.service.ts
@@ -8,14 +8,14 @@ import { UpdateTaskInput } from './dto/updateTask.input';
 export class TaskService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async getTasks(): Promise<Task[]> {
-    return await this.prismaService.task.findMany();
+  async getTasks(userId: number): Promise<Task[]> {
+    return await this.prismaService.task.findMany({ where: { userId } });
   }
 
   async createTask(createTaskInput: CreateTaskInput): Promise<Task> {
-    const { name, description } = createTaskInput;
+    const { name, description, userId } = createTaskInput;
     return await this.prismaService.task.create({
-      data: { name, description },
+      data: { name, description, userId },
     });
   }
 
