791001ca51ac5a959823b339ed659b1752258f03
masahiro96848 <feetwcpic9436@gmail.com>
2024-12-08 22:38:50 +0900

    タスクの削除機能を実装

--- api-server/src/task/task.service.ts
+++ api-server/src/task/task.service.ts
@@ -26,4 +26,10 @@ export class TaskService {
       where: { id },
     });
   }
+
+  async deleteTask(id: number): Promise<Task> {
+    return await this.prismaService.task.delete({
+      where: { id },
+    });
+  }
 }
